519567069e51c335c1246cee5df17903044cb578
Tilo Heidasch <tilo.heidasch@capgemini.com>
2020-06-06 17:48:58 +0200

    Removes swagger dependancy because of https://github.com/nestjs/nest/issues/1706

--- libs/api-interfaces/src/lib/api-interfaces.ts
+++ libs/api-interfaces/src/lib/api-interfaces.ts
@@ -1,8 +1,16 @@
 import { v4 as uuidv4 } from 'uuid';
-import { ApiProperty } from '@nestjs/swagger';
+//import { ApiProperty } from '@nestjs/swagger';
 export class CreateLlama {
-  @ApiProperty({ description: 'The name of the new Llama', example: 'Groceries' })
+  //@ApiProperty({ description: 'The name of the new Llama', example: 'Groceries' })
   name: string;
+}
+export class CreateItem {
+  //@ApiProperty({ description: 'The name of the new Item', example: 'Bananas' })
+  name: string;
+  //@ApiProperty({ description: 'The amount of the Item', example: '5', required: false })
+  amount?: string;
+  //@ApiProperty({ description: 'The unit of the amount of the Item', example: 'kg', required: false })
+  unit?: string;
 }
 export class Llama {
   id: string;
@@ -16,12 +24,22 @@ export class Llama {
     this.items = [];
   }
 }
-export interface Item {
+export class Item {
+  id: string;
+  created: Date;
   name: string;
+  done: boolean;
   amount?: string;
   unit?: string;
   category?: ItemCategory;
-  done?: boolean;
+  constructor(name: string, amount?: string, unit?: string) {
+    this.id = uuidv4();
+    this.created = new Date();
+    this.name = name;
+    this.amount = amount;
+    this.unit = unit;
+    this.done = false;
+  }
 }
 export interface ItemCategory {
   name: string;
