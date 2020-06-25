579a0b398c62e2e2b02ba46a5e3647ae457fa650
Tilo Heidasch <tilo.heidasch@capgemini.com>
2020-06-25 13:11:04 +0200

    Adds Only fetch emoji if text > 2

--- apps/llama/src/app/emoji.service.ts
+++ apps/llama/src/app/emoji.service.ts
@@ -12,24 +12,25 @@ export class EmojiService {
     let isFirst: boolean = true;
     const textBlocks: string[] = text.split(' ');
     for (let index = 0; index < textBlocks.length; index++) {
-      const textBlock = textBlocks[index];
-
-      const cacheKeyValue = this.cache.filter(keyValue => (keyValue.key === text));
-      if (cacheKeyValue[0] !== undefined) {
-        if (!isFirst) {
-          emoji += ' ';
-        }
-        emoji += cacheKeyValue[0].value;
-        isFirst = false;
-      } else {
-        const emojiArray: any = await this.http.get('https://emoji.th105.de/', { params: { "lang": "de", "max": "1", "minRelevance": "0.5", "search": textBlock } }).toPromise();
-        if (emojiArray !== undefined && emojiArray.length > 0) {
-          cacheKeyValue.push({ key: text, value: emojiArray[0].char });
+      const textBlock = textBlocks[index].trim();
+      if (textBlock.length > 2) {
+        const cacheKeyValue = this.cache.filter(keyValue => (keyValue.key === text));
+        if (cacheKeyValue[0] !== undefined) {
           if (!isFirst) {
             emoji += ' ';
           }
-          emoji += emojiArray[0].char;
+          emoji += cacheKeyValue[0].value;
           isFirst = false;
+        } else {
+          const emojiArray: any = await this.http.get('https://emoji.th105.de/', { params: { "lang": "de", "max": "1", "minRelevance": "0.5", "search": textBlock } }).toPromise();
+          if (emojiArray !== undefined && emojiArray.length > 0) {
+            cacheKeyValue.push({ key: text, value: emojiArray[0].char });
+            if (!isFirst) {
+              emoji += ' ';
+            }
+            emoji += emojiArray[0].char;
+            isFirst = false;
+          }
         }
       }
     }
